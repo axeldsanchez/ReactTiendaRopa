f5faf057b71ec4392a6745d47558bb1d1983dd21
Anabella <axel.d.sanchez@gmail.com>
2023-09-11 23:03:22 -0300

    Agregando local storage

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,28 +4,24 @@ import { Link } from "react-router-dom";
 
 
 export const Cart = () => {
-    const { cart, clearCart, totalQuantity, total } = useContext(CartContext)
+    const { cart, clearCart, total } = useContext(CartContext)
 
-    if (totalQuantity === 0) {
+    if (cart.length === 0) {
         return (
             <div>
                 <h2>No hay items en el Carrito</h2>
-                <Link to="/">Productos</Link>
+                <Link to="/"><button>Productos</button></Link>
             </div>
         )
     }
 
-    console.log(totalQuantity);
-
-
     return (
         <>
-            <div>
+            <div className="cartCarrito">
                 {cart.map(p =>
-                    <div className="card" style={{ width: '18rem' }}>
+                    <div className="card" key={p.id} style={{ width: '18rem' }}>
                         <div class="card-body">
                             <h5 class="card-title">{p.name}</h5>
-                            <p class="card-text">{p.description}</p>
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">Precio: ${p.price}</li>
@@ -33,11 +29,12 @@ export const Cart = () => {
                             <li class="list-group-item">Total: ${p.quantity * p.price}</li>
                         </ul>
                     </div>)}
+
+                <h3>Total: ${total()}</h3>
+                <button onClick={() => clearCart()}>Limpiar Carrito</button>
+                <Link to="/checkout"><button>Checkout</button></Link>
             </div>
 
-            <h3>Total: ${total}</h3>
-            <button onClick={() => clearCart()}>Limpiar Carrito</button>
-            <Link to="/checkout"><button>Checkout</button></Link>
         </>
     )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,18 +1,17 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
-export const CartContext = createContext({
-    cart:[]
-})
+export const CartContext = createContext()
+
+const cartInicial = JSON.parse(localStorage.getItem("cart") || "[]")
 
 export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState(cartInicial)
     console.log(cart)
 
     const addItem = (item, quantity) => {
         if(!isInCart(item.id)) {
             setCart (prev => [...prev, {...item, quantity}])
         } else { console.error("El producto ya fue agregado")
-
         }
     }
 
@@ -29,12 +28,23 @@ export const CartProvider = ({children}) => {
         return cart.some(prod => prod.id === itemId)
     }
 
+
+
     const totalQuantity = () => {
         return cart.reduce((acc, prod) => acc + prod.quantity, 0)
     }
 
+    const total = () => {
+        return cart.reduce((acc, prod) => acc + prod.price * prod.quantity, 0)
+    }
+
+
+    useEffect(()=>{
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }, [cart])
+
     return (
-        <CartContext.Provider value={{cart, addItem, removeItem, clearCart, totalQuantity}}>
+        <CartContext.Provider value={{cart, addItem, removeItem, clearCart, totalQuantity, total}}>
             {children}
         </CartContext.Provider>
     )
